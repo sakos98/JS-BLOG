eda2e3e9603b2210fb7418b6f7f2f81084aa018d
Michal Sak <michusakowicz@gmail.com>
2023-03-21 02:09:58 +0100

    I try JS

--- js/script.js
+++ js/script.js
@@ -12,17 +12,18 @@ for(let link of links){
 console.log(link);
 } */
 const titleClickHandler = function (event) {
-console.log('Link was clicked!');
-console.log(event);
+    event.preventDefault();
+    const clickedElement = this;
+    console.log('Link was clicked!');
 
-/* remove class 'active' from all article links  */
+/* [DONE] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 
 for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
 }
 
-/* add class 'active' to the clicked link */
+/*  [DONE] add class 'active' to the clicked link */
 const activeArticles = document.querySelectorAll('.post');
 
 for (let activeArticle of activeArticles) {
@@ -32,10 +33,19 @@ for (let activeArticle of activeArticles) {
 /* remove class 'active' from all articles */
 
 /* get 'href' attribute from the clicked link */
+console.log('clickedElement:', clickedElement);
+const articleSelector = document.getElementById("article-1");
+console.log(articleSelector);
 
 /* find the correct article using the selector (value of 'href' attribute) */
+const targetArticle = document.querySelector("#article");
+console.log(targetArticle);
 
-/* add class 'active' to the correct article */
+/* [IN PROGRESS] add class 'active' to the correct article */
+
+
+
+
 }
 
 const links = document.querySelectorAll('.titles a');
